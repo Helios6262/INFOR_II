6d1f014f9fe02ace4f7abf54b056b0c116c6326c
Helios Estevez Orozco <hestevezoro@unav.es>
2025-12-12 19:58:59 +0100

    inheritance lAB

---
+++ 10.1/Project1/employee.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class employee
+{
+	char name[40];
+public:
+	employee(char* n = (char*)"") {
+		strcpy_s(name, n); cout << "Employee constructor\n";}
+	void print_position(){
+		cout << " Position not defined" << endl;
+	}
+	char* GetName() { return name;}
+	~employee() { cout << "Employee destructor\n";};
+};
+
+class Manager : public employee
+{
+public:
+	Manager(char* n = (char*)"") {strcpy_s(name,n); cout << "Manager constructor\n";
+	}
+	void print_position() { cout << " is a Manager" << endl; }
+};
+
+class Worker : public employee
+{
+public:
+	Worker(char* n = (char*)"") { strcpy_s(name, n); cout << "Worker constructor\n";
+	}
+	void print_position() { cout << " is a Worker" << endl; }
+};
+
+class Officer : public Worker
+{
+public:
+	Officer(char* n = (char*)"") { strcpy_s(name, n); cout << "Officer constructor\n";
+	}
+	void print_position() { cout << " is an Officer" << endl;
+	}
+};
+class Technician : public Officer
+{
+public:
+	Technician(char* n = (char*)"") {
+		strcpy_s(name, n); cout << "Technician constructor\n";
+	}
+	void print_position() {cout << " is a Technician" << endl;
+	}
+};
--- 10.1/Project1/main.cpp
+++ 10.1/Project1/main.cpp
@@ -5,12 +5,21 @@ using namespace std;
 
 void main()
 {
-	Employee Rafa((char*)"Rafa");
+    employee Rafa((char*)"Rafa");
 	Manager Mario((char*)"Mario");
+	Worker Anton((char*)"Anton");
+	Officer Luis((char*)"Luis");
+	Technician Pablo((char*)"Pablo");
 
 	cout << "With object names:\n" << endl;
 	cout << Rafa.GetName();
 	Rafa.print_position();
 	cout << Mario.GetName();
 	Mario.print_position();
+	cout << Anton.GetName();
+	Anton.print_position();
+	cout << Luis.GetName();
+	Luis.print_position();
+	cout << Pablo.GetName();
+	Pablo.print_position();
 }
